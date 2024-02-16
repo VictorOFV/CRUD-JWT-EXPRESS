153ae11b0c84aa6ec5c910e7a1cf50987882cd0e
Victor Manoel Lopes Porto <victormanoellopesporto@gmail.com>
2024-02-16 02:52:50 -0400

    Added 2 new database schemas, Checklist and Task.

---
+++ src/database/models/Checklist.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const checklistSchema = new Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
+    author: { type: Schema.Types.ObjectId, ref: "User" }
+});
+
+const Checklist = mongoose.model("Checklist", checklistSchema);
+
+module.exports = Checklist;
---
+++ src/database/models/Task.model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const taskSchema = new Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    checklist: { type: Schema.Types.ObjectId, ref: "Checklist" }
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
